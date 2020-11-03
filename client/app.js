420b51b1766e4bf96aed2b627b9bae997974ce7a
belal-abuhani <belal.abuhani@gmail.com>
2020-11-03 19:05:43 +0200

    imp server + html

--- client/app.js
+++ client/app.js
@@ -1,2 +1,57 @@
+var text = document.getElementById("text-josn");
+var subBtn =document.getElementById("submit");
 
 
+var sendHttpRequest = (method, url, data) => {
+    var promise = new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.responseType = 'json';
+      if (data) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      }
+  
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      }
+  
+      xhr.onerror = () => {
+        reject('Something went wrong!');
+      }
+  
+      xhr.send(data);
+    })
+    return promise
+  }
+
+var sendJosn = (txtJosn) => {
+    sendHttpRequest('POST', 'http://127.0.0.1:3000/upload-json', txtJosn)
+      .then(responseData => {
+        console.log(responseData)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+var txtJosn={}
+var onChange = (e) => {
+    txtJosn = e.target.value
+    console.log(txtJosn)
+
+}
+
+
+ var onclick = (e)=>{
+        e.preventDefault()
+        console.log(txtJosn)
+        sendJosn(txtJosn)
+    }
+
+text.addEventListener("change",onChange)
+
+subBtn.addEventListener("click",onclick)
--- server.js
+++ server.js
@@ -1,2 +1,37 @@
 
 
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: false })) // parse the buffered data
+app.use(express.json()) // application/json
+app.use(express.static(__dirname +"/./client"))
+
+
+// app.get('/json' , (req,res)=>{
+
+// })
+
+app.post('/upload-json' ,(req,res)=>{
+    console.log(req.body)
+    res.send("Asd")
+
+})
+
+// app.post('/todo' , (req,res) =>{   //   (req,res)  ==> controller
+//     db.query( `select * from todo` , (err , todo) => { //  ==>   model
+//         if(err){
+//             res.sendStatus(404)
+//         }else{
+//             res.json(todo)
+//         }
+//     } )
+// })
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is lis...")
+})
+
